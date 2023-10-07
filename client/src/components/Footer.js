c96e1c5b36fbbf422878d0a713c54d38f3af7575
BIBHAB22 <bibhabindia@gmail.com>
2023-10-07 13:24:26 +0530

    Picture Update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Footer.css';
-import ritam from "./image/ritam.jpg"
-import bibhab from "./image/bibhab.jpg"
-import Dibyajyoti from "./image/Dibyajyoti.jpg"
+import ritam from "./image/profile pics/ritam.jpg"
+import bibhab from "./image/profile pics/bibhab.jpg"
+import Dibyajyoti from "./image/profile pics/Dibyajyoti.jpg"
 
 
 
