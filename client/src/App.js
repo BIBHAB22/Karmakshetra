033eae0494f008fd8f469317e683e15afd23836f
BIBHAB22 <bibhabindia@gmail.com>
2023-10-07 15:51:07 +0530

    vacation and incredible india page update

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,6 @@ import VacationPage from './components/Vacations';
 import Wildlife from './components/Wildlife';
 import BeachPage from './components/Beach';
 import IncredibleIndia from './components/IncredibleIndia';
-import Tajmahal from './components/Tajmahal';
-import Kerala from './components/Kerala';
-import DurgaPuja from './components/DurgaPuja';
-import Kashmir from './components/Kashmir';
 import TourismPage from './components/Dream_tour';
 
 const App = () => {
@@ -37,10 +33,6 @@ const App = () => {
           <Route path="/Wildlife" element={<Wildlife/>} className="Wildlife" /> 
           <Route path="/Beach.js" element={<BeachPage/>} className="Wildlife" />
           <Route path="/Incredible_India.js" element={<IncredibleIndia/>} className="Wildlife" />
-          <Route path="/blog/tajmahal" element={<Tajmahal/>} className="tajmahal" />
-          <Route path="/blog/kerala" element={<Kerala/>} className="tajmahal" />
-          <Route path="/blog/durgapuja" element={<DurgaPuja/>} className="tajmahal" />
-          <Route path="/blog/kashmir" element={<Kashmir/>} className="tajmahal" />
         </Routes>
     </>
   );
